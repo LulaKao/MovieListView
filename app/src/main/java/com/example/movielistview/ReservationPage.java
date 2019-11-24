53bf0c83e7aac3cbc591fee3890e093f153c4718
LulaKao <windandsprings@gmail.com>
2019-11-24 22:43:40 +0800

    first commit

---
+++ app/src/main/java/com/example/movielistview/ReservationPage.java
@@ -0,0 +1,13 @@
+package com.example.movielistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_page);
+    }
+}
